abf1d6d06ebb1f8c1868cf24e36e214444229525
Vedha585 <vedhanarayanan2002@gmail.com>
2024-10-17 23:29:52 +0530

    api endpoint

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, send, emit
+from flask import Flask, render_template, request
+from processInputQuery import getFinalAnswer
 
 app = Flask(__name__)
-socketio = SocketIO(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
 
-def calculate(msg):
-    return str(int(msg)**2)
+@app.route('/api/chat',methods=['POST'])
+def chatbot():
+    query = request.data.decode('utf-8')
+    response  = getFinalAnswer(query)
+    return response
 
-@socketio.on('message')
-def handle_message(msg):
-    print(f"Received message: {msg}")  # Log the message to the server console
-    ans = calculate(msg)
-    send(ans, broadcast=True)  # Broadcast the message to all connected clients
+# def calculate(msg):
+#     return str(int(msg)**2)
+
+# @socketio.on('message')
+# def handle_message(msg):
+#     print(f"Received message: {msg}")  # Log the message to the server console
+#     ans = calculate(msg)
+#     send(ans, broadcast=True)  # Broadcast the message to all connected clients
 
 if __name__ == '__main__':
-    socketio.run(app)
+     app.run(host='127.0.0.1',port=4444)
